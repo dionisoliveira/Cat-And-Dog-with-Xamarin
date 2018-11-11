4190cdd09dc86f0b83a3c4b14f9a6b88bceac78a
Dionis <dionisoliveira2gmail.com>
2018-11-11 21:52:23 -0200

    Adjust files

---
+++ CatAndDog/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using CatAndDogService.Services.Interface;
+using CatAndDogService.Services;
+
+namespace CatAndDog
+{
+    public partial class App : Application
+    {
+       
+        public App()
+        {
+
+
+            InitializeComponent();
+
+            RegisterIoC();
+
+            if (Device.RuntimePlatform == Device.iOS)
+               MainPage = new MainPage();
+            else
+                MainPage = new NavigationPage(new MainPage());
+
+           
+        }
+
+        /// <summary>
+        /// Register IoC
+        /// </summary>
+        private void RegisterIoC()
+        {
+            DependencyService.Register<ICatService, CatService>();
+            DependencyService.Register<IDogService, DogService>();
+        }
+    }
+}
---
+++ CatAndDog/Views/MainPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Xamarin.Forms;
+using CatAndDogService.Services.Interface;
+
+namespace CatAndDog
+{
+    public class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            Page catPage, dogPage = null;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+
+                    catPage= new NavigationPage(new CatPage())
+                    {
+                        
+                        Title = "Cat"
+                    };
+                    dogPage = new NavigationPage(new DogPage()
+                    {
+                        Title = "Dog"
+                    });
+
+                 
+                    break;
+                default:
+                    catPage = new CatPage()
+                    {
+
+                        Title = "Cat"
+                    };
+                    dogPage = new DogPage()
+                    {
+                        Title = "Dog"
+                    };
+                   
+                  
+                    break;
+            }
+
+       
+
+            Children.Add(catPage);
+            Children.Add(dogPage);
+         
+
+            Title = Children[0].Title;
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+            Title = CurrentPage?.Title ?? string.Empty;
+        }
+    }
+}
